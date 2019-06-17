79c0dc3bb794fb996619af978079334f8d7255e5
Dipo <mato.dipcar@gmail.com>
2019-06-17 14:54:46 +0200

    Update Reference.java

--- src/main/java/sk/dipo/money/utils/Reference.java
+++ src/main/java/sk/dipo/money/utils/Reference.java
@@ -5,6 +5,6 @@ public class Reference {
 	public static final String NAME = "Money Mod";
 	public static final String VERSION = "1.0.1";
 
-	public static final String PROXY_COMMON = "sk.dipo.money.CommonProxy";
-	public static final String PROXY_CLIENT = "sk.dipo.money.ClientProxy";
+	public static final String PROXY_COMMON = "sk.dipo.money.proxy.CommonProxy";
+	public static final String PROXY_CLIENT = "sk.dipo.money.proxy.ClientProxy";
 }
