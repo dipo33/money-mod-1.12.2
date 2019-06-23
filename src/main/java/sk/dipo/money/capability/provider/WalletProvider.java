6e77c759ea0bb64ac90f778b4ffa7c46eb650625
Dipo <mato.dipcar@gmail.com>
2019-06-24 01:58:33 +0200

    Create wallet capability

---
+++ src/main/java/sk/dipo/money/capability/provider/WalletProvider.java
@@ -0,0 +1,36 @@
+package sk.dipo.money.capability.provider;
+
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+public class WalletProvider implements ICapabilitySerializable<NBTBase> {
+
+	public static final int INV_SIZE = 27;
+	
+	private ItemStackHandler inventory = new ItemStackHandler(27);
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+		return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY;
+	}
+
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+		return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY ? CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.<T>cast(inventory) : null;
+	}
+
+	@Override
+	public NBTBase serializeNBT() {
+		return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.getStorage().writeNBT(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, this.inventory, null);
+	}
+
+	@Override
+	public void deserializeNBT(NBTBase nbt) {
+		CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.getStorage().readNBT(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, this.inventory, null, nbt);
+	}
+
+}
--- src/main/java/sk/dipo/money/handler/CapabilityHandler.java
+++ src/main/java/sk/dipo/money/handler/CapabilityHandler.java
@@ -1,12 +1,17 @@
 package sk.dipo.money.handler;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import sk.dipo.money.capability.provider.WalletProvider;
+import sk.dipo.money.utils.Reference;
 
 public class CapabilityHandler {
+	public static final ResourceLocation WALLET_CAPABILITY = new ResourceLocation(Reference.MODID, "wallet");
 
 	@SubscribeEvent
 	public void attachCapability(AttachCapabilitiesEvent<ItemStack> event) {
+		event.addCapability(WALLET_CAPABILITY, new WalletProvider());
 	}
 }
