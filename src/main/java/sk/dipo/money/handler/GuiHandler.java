717bef18494db3a37d9224009008d0f1fe1b3986
Dipo <mato.dipcar@gmail.com>
2019-06-24 01:52:15 +0200

    Add missing imports to GuiHandler

--- src/main/java/sk/dipo/money/handler/GuiHandler.java
+++ src/main/java/sk/dipo/money/handler/GuiHandler.java
@@ -1,5 +1,8 @@
 package sk.dipo.money.handler;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
 public class GuiHandler implements IGuiHandler {
 
 	@Override
