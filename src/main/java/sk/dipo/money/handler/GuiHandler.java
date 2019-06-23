f53b82f6ca1467da39df8ff39b6da02559984eb0
Dipo <mato.dipcar@gmail.com>
2019-06-24 01:51:13 +0200

    Create GuiHandler.java

---
+++ src/main/java/sk/dipo/money/handler/GuiHandler.java
@@ -0,0 +1,20 @@
+package sk.dipo.money.handler;
+
+public class GuiHandler implements IGuiHandler {
+
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		switch (ID) {
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		switch (ID) {
+		default:
+			return null;
+		}
+	}
+}
