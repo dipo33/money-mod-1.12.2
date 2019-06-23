22885906f473026ceae64b6135fb647c72beb39b
Dipo <mato.dipcar@gmail.com>
2019-06-24 01:59:53 +0200

    Open wallet GUI and handle it

--- src/main/java/sk/dipo/money/handler/GuiHandler.java
+++ src/main/java/sk/dipo/money/handler/GuiHandler.java
@@ -3,11 +3,26 @@ package sk.dipo.money.handler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import sk.dipo.money.container.ContainerWallet;
+import sk.dipo.money.gui.GuiWallet;
+
 public class GuiHandler implements IGuiHandler {
+
+	public static final int GUI_WALLET_MAIN = 0;
+	public static final int GUI_WALLET_OFF = 1;
 
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		IItemHandler cap;
 		switch (ID) {
+		case GUI_WALLET_MAIN:
+			cap = player.getHeldItemMainhand().getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+			return new ContainerWallet(player.inventory, cap);
+		case GUI_WALLET_OFF:
+			cap = player.getHeldItemOffhand().getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+			return new ContainerWallet(player.inventory, cap);
 		default:
 			return null;
 		}
@@ -15,7 +30,14 @@ public class GuiHandler implements IGuiHandler {
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		IItemHandler cap;
 		switch (ID) {
+		case GUI_WALLET_MAIN:
+			cap = player.getHeldItemMainhand().getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+			return new GuiWallet(player.inventory, cap);
+		case GUI_WALLET_OFF:
+			cap = player.getHeldItemOffhand().getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+			return new GuiWallet(player.inventory, cap);
 		default:
 			return null;
 		}
--- src/main/java/sk/dipo/money/item/ItemWallet.java
+++ src/main/java/sk/dipo/money/item/ItemWallet.java
@@ -1,6 +1,13 @@
 package sk.dipo.money.item;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import sk.dipo.money.MoneyMod;
+import sk.dipo.money.handler.GuiHandler;
 
 public class ItemWallet extends MoneyItem {
 
@@ -12,5 +19,20 @@ public class ItemWallet extends MoneyItem {
 	@Override
 	public int getMaxItemUseDuration(ItemStack stack) {
 		return 1;
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		if (!world.isRemote) {
+			if (hand == EnumHand.MAIN_HAND)
+				player.openGui(MoneyMod.instance, GuiHandler.GUI_WALLET_MAIN, world, player.getPosition().getX(), player.getPosition().getY(),
+						player.getPosition().getZ());
+			else
+				player.openGui(MoneyMod.instance, GuiHandler.GUI_WALLET_OFF, world, player.getPosition().getX(), player.getPosition().getY(),
+						player.getPosition().getZ());
+
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+		}
+		return new ActionResult<ItemStack>(EnumActionResult.PASS, player.getHeldItem(hand));
 	}
 }
