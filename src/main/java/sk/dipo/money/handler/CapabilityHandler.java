3d733f5c1c062b96df13e049e3cd076132d4657e
Dipo <mato.dipcar@gmail.com>
2019-06-24 01:53:17 +0200

    Create CapabilityHandler.java

---
+++ src/main/java/sk/dipo/money/handler/CapabilityHandler.java
@@ -0,0 +1,12 @@
+package sk.dipo.money.handler;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class CapabilityHandler {
+
+	@SubscribeEvent
+	public void attachCapability(AttachCapabilitiesEvent<ItemStack> event) {
+	}
+}
